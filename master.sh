e181fdfee737b225e1d71969b7671062343c83b6
Clay Shoaf <clayshoaf@gmail.com>
2022-10-05 15:05:29 -0400

    just testing

--- master.sh
+++ master.sh
@@ -1,4 +1,4 @@
 ./dateFileMaker.sh $1
 for i in $(ls dailydata/); do ./joiner.sh "dailydata/$i"; done
 ./colorizer.sh dailydata/*.csv
-sed -i '1s/^/name,longitude,latitude,color\n/' dailydata/*.csv
+sed -i '1s/^/name,latitude,longitude,color\n/' dailydata/*.csv

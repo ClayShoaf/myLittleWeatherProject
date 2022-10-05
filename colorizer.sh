9c23cd4d74dab8a2a4d80ca7643123cbf3525bbe
Clay Shoaf <clayshoaf@gmail.com>
2022-10-05 16:12:12 -0400

    updated scripts and output files

--- colorizer.sh
+++ colorizer.sh
@@ -1,1 +1,2 @@
+gawk -i inplace -F, '{print $1 ": " $4 "," $2 "," $3 "," $4}' $1
 for i in $(cut -d',' -f4 $1); do sed -i -e s/,$i$/,$(./run.sh $i)/g $1; done
--- master.sh
+++ master.sh
@@ -1,4 +1,8 @@
+echo "making files"
 ./dateFileMaker.sh $1
+echo "joining files"
 for i in $(ls dailydata/); do ./joiner.sh "dailydata/$i"; done
-./colorizer.sh dailydata/*.csv
+echo "colorizing files"
+for i in $(ls dailydata/); do ./colorizer.sh "dailydata/$i"; done
+echo "adding headers to files"
 sed -i '1s/^/name,latitude,longitude,color\n/' dailydata/*.csv
